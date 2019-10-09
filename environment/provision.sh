1106fbfe43a51b64b6a392132f3d55e838563134
Rory Stokes <rstokes@spartaglobal.com>
2019-10-09 21:37:34 +0100

    made vagrant file and provision shell

---
+++ environment/provision.sh
@@ -0,0 +1,9 @@
+#!/bin/#!/usr/bin/env bash
+
+sudo apt-get update -y
+
+sudo apt-get install nginx -y
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs
